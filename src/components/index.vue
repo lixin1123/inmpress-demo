44edb789184495494f17a9ce1b8a0908f31b6fbf
lixin1123 <llixin1123@163.com>
2021-09-27 14:34:26 +0800

    test

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,40 +1,76 @@
 <template>
-  <canvas id="canvas" width="600" height="600" style="border:1px solid #ccc"></canvas>
+  <canvas id="canvas" width="2500" height="800" style="border:1px solid #ccc"></canvas>
 </template>
 
 <script>
 import { fabric } from 'fabric'
-import { onMounted, reactive } from 'vue';
+import { reactive } from 'vue';
 export default {
   name:'Demo',
   setup() {
-   const data = reactive({
-     x: 0, 
-     y: 0
-   })
-   onMounted(() => {
-    var canvas = new fabric.Canvas('canvas');
-    fabric.Object.prototype.transparentCorners = false;
-
-    var rect = new fabric.Rect({
-    left: 60,
-    top: 10,
-    width: 100,
-    height: 100,
-    fill: 'red',
-    angle: 45,
-    padding: 10,
-    stroke: 'green', // 边框颜色
-    strokeWidth: 4, // 边框宽度
-    });
-    const findObject = (x, y) => {
-      canvas._objects.forEach(element => {
-        element.animate({opacity: 0} , {
-          duration: 2000,
-          easing: fabric.util.ease.easeOutCubic,
-          onChange: canvas.renderAndResetBound,
-        });
-        if(element.left < x && element.left + element.width > x && element.top < y && element.top + element.height > y ) {
+    const data = reactive({
+      x: 0, 
+      y: 0,
+    })
+    let items = []
+    //1.获取数据
+    getData()
+    function getData() {
+      const xhr = new XMLHttpRequest()
+      xhr.open('get','/api/getData')
+      // xhr.responseType = 'document'
+      xhr.send()
+      xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4) {
+          if(xhr.status === 200) {
+            items = JSON.parse(xhr.responseText).data.scenes[0].elements
+            console.log(items)
+            canvas()
+            
+          }else{
+            console.error(xhr.statusText)
+          }
+        }
+      }
+    }
+    //2.获取数据之后渲染canvas
+    function canvas() {
+      var canvas = new fabric.Canvas('canvas')
+      fabric.Object.prototype.transparentCorners = false
+      items.forEach(item => {
+        switch (item.type) {
+          case 'Image':
+            fabric.Image.fromURL(item.url, (img, err) => {
+              if(!err) {
+                img.set({
+                  left: item.position.x, // 左上角位置
+                  top: item.position.y, // 左上角位置
+                  width: item.position.width,
+                  height: item.position.height,
+                  target: false,
+                  crossOrigin: 'anonymous' // 使用的图片跨域时，配置此参数，有时会失效
+                })
+                console.log(item.position.x)
+                canvas.add(img)
+              }
+            })
+            break
+          case 'Text':
+            canvas.add(addText(item))
+            // text.enterEditing() 
+            break
+        }
+      })
+      const findObject = (x, y) => {
+        canvas._objects.forEach(element => {
+          if(element.left < x && element.left + element.width > x && element.top < y && element.top + element.height > y && element.target ) {
+            canvas._objects.forEach(element => {
+              element.animate({opacity: 0} , {
+              duration: 200,
+              easing: fabric.util.ease.easeOutCubic,
+              onChange: canvas.renderAndResetBound,
+            })
+          })
           var rect2 = new fabric.Rect({
             left: 60,
             top: 10,
@@ -50,64 +86,28 @@ export default {
             canvas.clear()
             var canvas1 = new fabric.Canvas('canvas');
             canvas1.add(rect2)
-          },2000)          
+          },200)          
         }
-      });
-    }
-    var rect1 = new fabric.Rect({
-      left: 140,
-      top: 120,
-      width: 150,
-      height: 100,
-      fill: 'red',
-      angle: 0,
-      padding: 10,
-      stroke: 'green', // 边框颜色
-      strokeWidth: 4, // 边框宽度
-    });
-    canvas.add(rect, rect1);
-    canvas.on({
-      'mouse:down': function(e) {
-        findObject(e.pointer.x, e.pointer.y)
-        console.log('111')
+      })
       }
-    })
-  })
-  return {
-    data
-  }
-  // fabric.loadSVGFromURL('../assets/logo.png', function(objects, options) {
-
-  //   var shape = fabric.util.groupSVGElements(objects, options);
-  //   canvas.add(shape.scale(0.6));
-  //   shape.set({ left: 200, top: 100 }).setCoords();
-  //   canvas.renderAll();
-
-  //   canvas.forEachObject(function(obj) {
-  //     var setCoords = obj.setCoords.bind(obj);
-  //     obj.on({
-  //       moving: setCoords,
-  //       scaling: setCoords,
-  //       rotating: setCoords
-  //     });
-  //   })
-  // });
-
-  // canvas.on('after:render', function() {
-  //   canvas.contextContainer.strokeStyle = '#555';
-
-  //   canvas.forEachObject(function(obj) {
-  //     var bound = obj.getBoundingRect();
-
-  //     canvas.contextContainer.strokeRect(
-  //       bound.left + 0.5,
-  //       bound.top + 0.5,
-  //       bound.width,
-  //       bound.height
-  //     );
-  //   })
-  // });
-
+    
+      canvas.on({'mouse:down': function(e) {
+        findObject(e.pointer.x, e.pointer.y)
+      }})
+    }
+    const addText =  (item) =>{
+      let text = new fabric.Textbox(item.text, {
+        left: item.position.x, // 左上角位置
+        top: item.position.y, // 左上角位置
+        fontSize: item.font.fontSize,
+        fill: item.font.color, // 字体颜色
+        target: false,
+      })
+      return text
+    }
+    return {
+      data
+    }
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
+const express = require('express')
+const app = express()
+
+var data = require('./mock/data')  //本地json文件数据
+
+var apiRoutes = express.Router();
+app.use('/api', apiRoutes)
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/impress-demo/'
@@ -7,4 +14,20 @@ module.exports = {
       impress: "impress",
     },
   },
+  devServer: {
+    before(app) {
+      app.get('/api/getData', (req,res) => {
+        res.json({
+          errno: 0,
+          data: data
+        })
+      })
+    },
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 8080,
+    https: false,
+    hotOnly: false,
+    proxy: null
+  }
 };
